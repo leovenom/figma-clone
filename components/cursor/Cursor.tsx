b8000ba374e5d5f3752b298f6939d53bc047aeee
leovenom <leonardt@outlook.com>
2024-03-11 14:12:04 +0000

    fix

--- components/cursor/Cursor.tsx
+++ components/cursor/Cursor.tsx
@@ -11,7 +11,7 @@ const Cursor = ({ color, x, y, message }: Props) => {
   return (
     <div
       className="pointer-events-none absolute top-0 left-0"
-      style={{ transform: `translateX(${x}px) translateY(${y})` }}
+      style={{ transform: `translateX(${x}px) translateY(${y}px)` }}
     >
       <CursorSVG color={color} />
 
